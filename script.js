fab11baa35b87a7ed653d43038463bdae8156535
Casey Waweru <casey.waweru@gmail.com>
2025-09-01 12:19:49 +0300

    Deleting Poem.txt

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello there");
+// Enable Color Change with mouse hover function
+const colorchange = document.getElementsByTagName("hover");
+
+/* colorchange.addEventListener("mouseover", (event) => {
+  event.target.style.color = "purple";
+
+  setTimeOut(() => {
+  event.target.style.color = "orange";
+  }, 5000);
+})
+
+onmouseover = (event) => {} */
+
+document.getElementById("My paragraph").onmouseover = function()
+{
+    this.style.backgroundColor = "black";
+}
